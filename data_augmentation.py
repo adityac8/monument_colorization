50b364ab14e70fde6555c096209dc83217956b0e
Aditya Arora <adityadvlp@gmail.com>
2018-07-17 04:16:55 +0530

    Rotation done

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,22 +1,23 @@
 # Random Rotations
-from keras.datasets import mnist
 from keras.preprocessing.image import ImageDataGenerator
 from keras import backend as K
-K.set_image_data_format('channels_first')
+K.set_image_data_format('channels_last')
+import cv2
+from glob import glob
+import numpy as np
 
-# load data
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+x=[]
+for i in glob("pisaDataset/*/*.jpg"):
+    im = cv2.imread(i)
+    x.append(im)
+    
+x=np.array(x)
+x = x.astype('float32')
 
-# reshape to be [samples][pixels][width][height]
-X_train = X_train.reshape(X_train.shape[0], 1, 28, 28)
-X_test = X_test.reshape(X_test.shape[0], 1, 28, 28)
-# convert from int to float
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
 # define data preparation
 datagen = ImageDataGenerator(rotation_range=90)
 # fit parameters from data
-datagen.fit(X_train)
+datagen.fit(x)
 # configure batch size and retrieve one batch of images
-for i in datagen.flow(X_train, save_to_dir='images',batch_size=9, save_prefix='aug', save_format='jpg'):
+for i in datagen.flow(x, save_to_dir='images',batch_size=9, save_prefix='aug', save_format='jpg'):
     pass
