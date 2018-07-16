81452168312609b533822086b4cf2bf6883c7648
Aditya Arora <adityadvlp@gmail.com>
2018-07-17 01:43:36 +0530

    Inbuilt save option

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,11 +1,9 @@
 # Random Rotations
 from keras.datasets import mnist
 from keras.preprocessing.image import ImageDataGenerator
-from matplotlib import pyplot
 from keras import backend as K
 K.set_image_data_format('channels_first')
 
-from PIL import Image
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
@@ -20,10 +18,5 @@ datagen = ImageDataGenerator(rotation_range=90)
 # fit parameters from data
 datagen.fit(X_train)
 # configure batch size and retrieve one batch of images
-#for X_batch, y_batch in 
-j=0
-from skimage.io import imsave
-for i in datagen.flow(X_train, save_to_dir='images',batch_size=1, save_prefix='aug', save_format='jpg'):
-    j+=1
-#    im = Image.fromarray(i)
-    imsave("your_file"+str(j)+".jpg",i[0][0].astype('int32'))
+for i in datagen.flow(X_train, save_to_dir='images',batch_size=9, save_prefix='aug', save_format='jpg'):
+    pass
